624e4489867643897c9a1fc6797211d4ffb1656a
justgri <justinas.grigaitis@gmail.com>
2025-10-23 19:57:06 +0200

    updates to path

--- app/app.py
+++ app/app.py
@@ -6,7 +6,6 @@ st.set_page_config("Class Sample Project", page_icon="🐍", layout="wide")
 
 
 home_page = st.Page("pages/homepage.py", title="Homepage")
-
 data_page = st.Page("pages/data_overview.py")
 
 user_pages = [home_page, data_page]
