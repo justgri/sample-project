8e8c0ba041d5511c5447a2a04394248e34128566
justgri <justinas.grigaitis@gmail.com>
2025-10-23 15:29:09 +0200

    fix relative path

--- app/app.py
+++ app/app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+from pathlib import Path
+
 
 st.title("Sample Project")
 
@@ -8,7 +10,12 @@ st.header("Hello world!")
 st.markdown("Some additional text")
 
 
+ROOT = Path(__file__).parent  # .../app
+df_disney = pd.read_csv(ROOT / "data" / "disney_movies_clean.csv")
+
+
 # df_disney = pd.read_csv("data/disney_movies_clean.csv")
-df_disney = pd.read_csv("app/data/disney_movies_clean.csv")
+# df_disney = pd.read_csv("app/data/disney_movies_clean.csv")
+
 
 st.dataframe(df_disney)
