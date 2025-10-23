53d3e16ab59ef9fea3a6edff970acb8e82c529d4
justgri <justinas.grigaitis@gmail.com>
2025-10-23 15:44:59 +0200

    add password

--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from pathlib import Path
 
+st.set_page_config("Class Sample Project", page_icon="🐍", layout="wide")
 
 st.title("Sample Project")
 
@@ -13,23 +14,31 @@ st.markdown("Some additional text")
 ROOT = Path(__file__).parent  # .../app
 df_disney = pd.read_csv(ROOT / "data" / "disney_movies_clean.csv")
 
-
-# df_disney = pd.read_csv("data/disney_movies_clean.csv")
-# df_disney = pd.read_csv("app/data/disney_movies_clean.csv")
-
-
-st.dataframe(df_disney)
-
 st.header("Project Intro")
 
 st.header("Data overview")
 
-col_data, _, col_chart = st.columns((0.8, 0.05, 1))
 
-with col_data:
-    st.subheader("Raw Data")
-    st.dataframe(df_disney)
+with st.sidebar:
+    user_input_password = st.text_input("Enter your password:", width=300)
 
-with col_chart:
-    st.subheader("Data Overview")
-    st.markdown("Placeholder for chart")
+
+true_pw = st.secrets["true_password"]
+
+st.write(true_pw)
+
+st.header(user_input_password)
+
+if user_input_password == true_pw:
+
+    st.subheader("This is a secret page")
+
+    col_data, _, col_chart = st.columns((0.8, 0.05, 1))
+
+    with col_data:
+        st.subheader("Raw Data")
+        st.dataframe(df_disney)
+
+    with col_chart:
+        st.subheader("Data Overview")
+        st.markdown("Placeholder for chart")
