f126938771653123247c5e36f1d1d918171c34c5
justgri <justinas.grigaitis@gmail.com>
2025-10-23 20:05:02 +0200

    add requirements

--- app/app.py
+++ app/app.py
@@ -4,9 +4,8 @@ from pathlib import Path
 
 st.set_page_config("Class Sample Project", page_icon="🐍", layout="wide")
 
-
-home_page = st.Page("pages/homepage.py", title="Homepage")
-data_page = st.Page("pages/data_overview.py")
+home_page = st.Page("pages/homepage.py", icon="🏠", title="Homepage")
+data_page = st.Page("pages/data_overview.py", icon="📊", title="Data Overview")
 
 user_pages = [home_page, data_page]
 
--- app/pages/homepage.py
+++ app/pages/homepage.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 
 st.title("Sample Project")
@@ -10,17 +11,39 @@ st.header("Hello world!")
 st.markdown("Some additional text")
 
 
-ROOT = Path(__file__).parent.parent  # .../app
+# define relative path
+# note the double parent, since this file is in pages folder
+ROOT_APP = Path(__file__).parent.parent
+st.write(ROOT_APP)
 
-st.write(ROOT)
+df_disney = pd.read_csv(ROOT_APP / "data" / "disney_movies_clean.csv")
 
 
-df_disney = pd.read_csv(ROOT / "data" / "disney_movies_clean.csv")
+st.header("Introduction")
 
-st.header("Project Intro")
+st.markdown("This is some text describing what my project is about")
+
+st.markdown(
+    """My data contains the following variables:
+1. Variable A
+2. Variable B
+3. Variable C
+""",
+    unsafe_allow_html=True,
+)
+
+st.markdown(
+    """These are the main questions I want to answer:
+1. Questions A
+2. Questions B
+3. Questions C
+""",
+    unsafe_allow_html=True,
+)
+
 
 st.header("Data overview")
-
+st.dataframe(df_disney)
 
 with st.sidebar:
     user_input_password = st.text_input("Enter your password:", width=300)
@@ -31,14 +54,17 @@ true_pw = st.secrets["true_password"]
 
 if user_input_password == true_pw:
 
-    st.subheader("This is a secret page")
+    st.title("This is a secret insights page")
 
     col_data, _, col_chart = st.columns((0.8, 0.05, 1))
 
     with col_data:
-        st.subheader("Raw Data")
-        st.dataframe(df_disney)
+        st.subheader("My unique ideas")
+        st.markdown("Placeholder for unique ideas")
 
     with col_chart:
-        st.subheader("Data Overview")
-        st.markdown("Placeholder for chart")
+        st.subheader("Movie imdb votes")
+        fig, ax = plt.subplots(figsize=(8, 4))
+
+        ax.hist(df_disney["imdb_votes"])
+        st.pyplot(fig)
