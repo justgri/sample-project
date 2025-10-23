82ccf8c82219b4f4d32af6949ea12f49f291b02e
justgri <justinas.grigaitis@gmail.com>
2025-10-23 15:31:32 +0200

    add first content

--- app/app.py
+++ app/app.py
@@ -19,3 +19,17 @@ df_disney = pd.read_csv(ROOT / "data" / "disney_movies_clean.csv")
 
 
 st.dataframe(df_disney)
+
+st.header("Project Intro")
+
+st.header("Data overview")
+
+col_data, _, col_chart = st.columns((0.8, 0.05, 1))
+
+with col_data:
+    st.subheader("Raw Data")
+    st.dataframe(df_disney)
+
+with col_chart:
+    st.subheader("Data Overview")
+    st.markdown("Placeholder for chart")
